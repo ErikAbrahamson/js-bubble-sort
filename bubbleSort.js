4c9155f9893ad536acc7923e461a33ef75e1df9d
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-31 12:41:30 -0600

    solve bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,5 +1,12 @@
-function bubbleSort(arr){
-
+function bubbleSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[i + 1]) {
+            var temp = arr[i + 1];
+            arr[i + 1] = arr[i];
+            arr[i] = temp;
+            i = i - 2;
+        }
+    }
+    return arr;
 }
-
 module.exports = bubbleSort;
